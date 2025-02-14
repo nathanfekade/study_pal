4efc9f891ea6e61160edf375e2789d393ff0fae2
Nathan Fekade <nathanfekade276@gmail.com>
2025-02-14 13:43:46 +0300

    added swagger and an endpoint for checking

--- study_pal/settings.py
+++ study_pal/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework_swagger',
+    'drf_yasg',
+    'rest_framework',
+    'study_space',
+    'user'
 ]
 
 MIDDLEWARE = [
