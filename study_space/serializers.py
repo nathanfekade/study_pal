4ddfb7c3f9bd6b016ae3bf34e266daa06dc83501
Nathan Fekade <nathanfekade276@gmail.com>
2025-02-14 17:25:37 +0300

    created serializer for Book and Questionairre model

---
+++ study_space/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from study_space.models import Book, Questionairre
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = '__all__'
+        read_only_fields = ['user']
+
+class QuestionairreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Questionairre
+        fields = '__all__'
+        read_only_fields = ['user']
+
